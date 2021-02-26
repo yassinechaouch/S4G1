a4ee7eeb5a6027828ee66afd547850720fd743f6
Mohamed Ghaith Alioua <gaith.alioua@hotmail.com>
2021-02-26 16:19:27 +0100

    Added Sensor

--- Sensor.py
+++ Sensor.py
@@ -8,7 +8,8 @@ Sensor = 8
 GPIO.setup(Led, GPIO.OUT)
 GPIO.setup(Sensor, GPIO.IN)
 
-if GPIO.input(Sensor):
-    GPIO.output(Led,GPIO.HIGH)
-else:
-    GPIO.output(Led, GPIO.LOW)
+while True:
+    if GPIO.input(Sensor):
+        GPIO.output(Led,GPIO.HIGH)
+    else:
+        GPIO.output(Led, GPIO.LOW)
