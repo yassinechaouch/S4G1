b9638f09b5fc4249a06023413639393d26a41f2c
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2021-04-08 12:47:26 +0100

    modified initial code

--- ROADENT/App/motor.py
+++ ROADENT/App/motor.py
@@ -5,18 +5,18 @@ def forward(tf):
     init()
     PWM = GPIO.PWM(Power, 100)  '100% power supplied'
     PWM.start(0)
-    GPIO.output(Motor_1, True)
-    GPIO.output(Motor_2, True)
-    GPIO.output(Motor_3, True)
-    GPIO.output(Motor_4, True)
+    GPIO.output(Motor_A+, True)
+    GPIO.output(Motor_A-, False)
+    GPIO.output(Motor_B+, True)
+    GPIO.output(Motor_B-, False)
     time.sleep(tf)
 
 def forward_slow(tf):
     init()
     PWM = GPIO.PWM(Power, 20) '20% power supplied'
     PWM.start(0)
-    GPIO.output(Motor_1, True)
-    GPIO.output(Motor_2, True)
-    GPIO.output(Motor_3, True)
-    GPIO.output(Motor_4, True)
+    GPIO.output(Motor_A+, True)
+    GPIO.output(Motor_A-, False)
+    GPIO.output(Motor_B+, True)
+    GPIO.output(Motor_B-, False)
     time.sleep(tf)
