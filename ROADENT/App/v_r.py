71d884f2fd41d9d9124f98d5f57287df66972646
Mohammad Ashraf Zohdi Ahmad <mohammadashraf2001@gmail.com>
2021-05-19 15:57:38 +0100

    Started on Speech recognition

--- ROADENT/App/v_r.py
+++ ROADENT/App/v_r.py
@@ -8,5 +8,12 @@ with harvard as source:
     audio = r.record(source)
 
 print(type(audio))
-
 print(r.recognize_google(audio))
+
+jackhammer = sr.AudioFile('audio_files_jackhammer.wav')
+with jackhammer as source1:
+    r.adjust_for_ambient_noise(source1, duration=0.5)
+    audio1 = r.record(source1)
+
+print('                            ')
+print(r.recognize_google(audio1))
