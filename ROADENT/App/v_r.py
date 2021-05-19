a5b187594d8ab5b943ebfbad5c0272c8c1c1c520
Mohammad Ashraf Zohdi Ahmad <mohammadashraf2001@gmail.com>
2021-05-19 15:47:35 +0100

    Started on Speech recognition

--- ROADENT/App/v_r.py
+++ ROADENT/App/v_r.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-sr.__version__
+print(sr.__version__)
 
 r = sr.Recognizer()
 
@@ -7,4 +7,6 @@ harvard = sr.AudioFile('audio_files_harvard.wav')
 with harvard as source:
     audio = r.record(source)
 
-r.recognize_google(audio)
+print(type(audio))
+
+print(r.recognize_google(audio))
