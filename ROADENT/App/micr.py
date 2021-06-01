35815dbcd3992fcb4a5762db440fb838ef97d732
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2021-06-01 11:25:53 +0100

    mic file

--- ROADENT/App/micr.py
+++ ROADENT/App/micr.py
@@ -1,17 +1,21 @@
 import speech_recognition as sr
-r = sr.Recognizer()
 
-mic = sr.Microphone()
+def mic():
+    r = sr.Recognizer()
 
-#sr.Microphone.list_microphone_names()
+    mic = sr.Microphone()
 
-#mic = sr.Microphone(device_index=3)
+    #sr.Microphone.list_microphone_names()
 
-with mic as source:
+    mic = sr.Microphone(device_index=3)
+
+    with mic as source:
     audio = r.listen(source)
 
-r.recognize_google(audio)
+    r.recognize_google(audio)
 
-'''with mic as source:
+    with mic as source:
     r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)'''
+    audio = r.listen(source)
+
+    return audio
