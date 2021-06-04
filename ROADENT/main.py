f1d096b156c305a43240306a4b0c2c8422f6f284
Mohamed Ghaith Alioua <gaith.alioua@hotmail.com>
2021-06-04 14:39:37 +0100

    ...

--- ROADENT/main.py
+++ ROADENT/main.py
@@ -1,6 +1,10 @@
 from ROADENT.App.Voice_Commands import Voice_Commands
-
+from ROADENT.App.motors import *
+from ROADENT.App import *
 if __name__ == "__main__":
 
     while True:
-        Voice_Commands()
+        Motor_1.clockwise()
+     #   Voice_Commands()
+
+
