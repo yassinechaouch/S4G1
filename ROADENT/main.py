148352239550c8549c391cd94d6edca21b0f2fce
Mohamed Ghaith Alioua <gaith.alioua@hotmail.com>
2021-06-08 13:34:38 +0100

    changed pins

--- ROADENT/main.py
+++ ROADENT/main.py
@@ -1,10 +1,28 @@
-from ROADENT.App.Voice_Commands import Voice_Commands
-from ROADENT.App.motors import *
-from ROADENT.App import *
-if __name__ == "__main__":
+#from ROADENT.App.Voice_Commands import Voice_Commands
+#from ROADENT.App.motors import *
+#from ROADENT.App import *
 
-    while True:
-        Motor_1.clockwise()
+#if __name__ == "__main__":
+
+    #while True:
+     #   Motor_1.clockwise()
      #   Voice_Commands()
+import speech_recognition as sr
+
+if __name__ == "__main__":
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    sr.Microphone.list_microphone_names()
+    mic = sr.Microphone(device_index=3)
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    x = r.recognize_google(audio)
+
+    print(x)
+
+
 
 
