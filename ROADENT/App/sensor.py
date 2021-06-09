524fcc74e9473f89f56ffc7378e345e598c6dede
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2021-06-09 15:32:09 +0100

    sensor innit file

--- ROADENT/App/sensor.py
+++ ROADENT/App/sensor.py
@@ -45,3 +45,36 @@ class Sensor():
         if (distance < 0.1):
             Motor_1.stop() '''
 
+import RPi.GPIO as GPIO
+
+ObstaclePin = 24
+
+#def __init__(self, echo_pin, trigger_pin):
+def setup():
+    GPIO.setmode(GPIO.BOARD)  # Set GPIO by numbers
+
+
+GPIO.setup(ObstaclePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+def loop():
+    while True:
+        if (0 == GPIO.input(ObstaclePin)):
+
+
+print
+"14CORE | Obstacle Avoidance Sensor Test \n"
+print
+" DETECTED: There is an obstacle ahead"
+
+
+def destroy():
+    GPIO.cleanup()  # Release resource
+
+
+'''if __name__ == '__main__':  # The Program start here
+    setup()
+try:
+    loop()
+except KeyboardInterrupt:  # Control C is pressed, the child program destroy will be executed.
+    destroy()
